6dafc16c1fa4e5d82b80e53ff83f49ba7edd1412
naoya <naoyama88@gmail.com>
2019-04-04 00:33:36 -0700

    test

--- app/Services/Line/Event/UnfollowService.php
+++ app/Services/Line/Event/UnfollowService.php
@@ -37,17 +37,14 @@ class UnfollowService
             DB::beginTransaction();
 
             $line_id = $event->getUserId();
-            $rsp = $this->bot->getProfile($line_id);
-            if (!$rsp->isSucceeded()) {
-                logger()->info('failed to get profile. skip processing.');
+            if (empty($line_id)) {
+                logger()->info('failed to get line id. skip processing.');
                 return false;
             }
 
-            $profile = $rsp->getJSONDecodedBody();
             $line_friend = new LineFriend();
             $input = [
                 'line_id' => $line_id,
-                'display_name' => $profile['displayName'],
             ];
 
             $line_friend->fill($input)->delete();
