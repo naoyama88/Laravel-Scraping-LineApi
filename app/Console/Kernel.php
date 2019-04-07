dfc2b7b9ee1752594c5bcd2fab69414c63c620be
naoya <naoyama88@gmail.com>
2019-04-07 12:50:11 -0700

    test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -37,6 +37,8 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
+
+        error_log('error_log function1');
         $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
