43f908ab9951885ad72c212241a37d393110c33c
naoya <naoyama88@gmail.com>
2019-04-13 17:06:58 -0700

    fix typo

--- database/migrations/2019_04_13_230529_create_jobs_table.php
+++ database/migrations/2019_04_13_230529_create_jobs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateJobTable extends Migration
+class CreateJobsTable extends Migration
 {
     /**
      * Run the migrations.
