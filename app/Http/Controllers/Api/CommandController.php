308d3feb51bdac463d64e3eb243f5147a5cf88ca
naoyama88 <naoyama88@gmail.com>
2019-09-03 19:10:10 -0700

    change text to inform cloing service

--- app/Http/Controllers/Api/CommandController.php
+++ app/Http/Controllers/Api/CommandController.php
@@ -13,7 +13,7 @@ class CommandController
     {
         $bot = app('line-bot');
         $lineId = 'U77aca8442a34fea506dfc9990738d242';
-        $textMessageBuilder = new TextMessageBuilder('Test message.');
+        $textMessageBuilder = new TextMessageBuilder('JPCANADAバンクーバーお仕事配信botをご利用いただきありがとうございます。開発者Twitterをご覧の方はご存知と思いますが、LINEDevelopersアカウントの関係で不具合が起きており、botでのお仕事情報配信が困難な状態になったため、こちらのbotの運用開発を中止いたします。ご利用の方にはご迷惑をおかけし申し訳ありません。botを友達削除する場合は、botのメーッセージを削除した後botアカウントをブロックし、ブロックリストから削除してください。');
         $response = $bot->pushMessage($lineId, $textMessageBuilder);
     }
 }
